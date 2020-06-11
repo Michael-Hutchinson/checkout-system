29e6df00a1d506109b5e045594d011e32013008d
Michael Hutchinson <michaelhutchinson@michaels-mbp.mynet>
2020-06-11 16:05:19 +0100

    footer styling

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -3,7 +3,9 @@ import React from 'react';
 function Footer() {
   return (
     <footer>
+      <div className="container">
         <p>CDL Tech Test - 2020</p>
+      </div>
     </footer>
   );
 }
